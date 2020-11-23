4b24e47b6fd1003d9931514ffa506db0f4b6acbe
poverholt <poverholt@gmail.com>
2020-11-23 08:24:00 -0600

    First commit

---
+++ src/clj/guestbook/routes/home.clj
@@ -0,0 +1,27 @@
+(ns guestbook.routes.home
+  (:require
+   [guestbook.layout :as layout]
+   [guestbook.db.core :as db]
+   [clojure.java.io :as io]
+   [guestbook.middleware :as middleware]
+   [ring.util.http-response :as response]))
+
+(defn home-page [request]
+  (layout/render
+  request "home.html" {:messages (db/get-messages)}))
+
+(defn save-message! [{:keys [params]}]
+  (db/save-message! params)
+  (response/found "/"))
+  
+(defn about-page [request]
+  (layout/render request "about.html"))
+
+(defn home-routes []
+  [""
+   {:middleware [middleware/wrap-csrf
+                 middleware/wrap-formats]}
+   ["/" {:get home-page}]
+   ["/message" {:post save-message!}]
+   ["/about" {:get about-page}]])
+
